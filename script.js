e7cdf8c194055b6db16880c7964ad43a0818f32b
Dan Bushong <bushongdan@gmail.com>
2021-01-10 13:03:29 -0600

    columns rows and correct time displayed on page

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 
+//grabs local storage
+function getLocalStorage(key) {
 
+    let value = localStorage.getItem(key);
+    //if there is something there in the key
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
 //what will happen when the page loads
 $(document).ready(function () {
 
@@ -12,12 +20,12 @@ $(document).ready(function () {
 
         // creates the 9 rows and names them their time 9-17
         var row = $(`<div data-time=${i} id='${i}' class="row">`);
-        
+
         // make first column
         var col1 = $('<div class="col-sm-2"> <p class="hour">' + amPmCorrect(i) + '</p>');
 
         //make second column and make the text id the same as what i is when it is being created
-        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add event"></textarea>`);
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);
 
         //make third column 
         var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
@@ -30,8 +38,23 @@ $(document).ready(function () {
         // puts the row with columns into the container in the html
         $(".container").append(row);
 
-        
+        getLocalStorage(i);
     }
 
-    
+    function amPmCorrect(hours) {
+
+        //if more than 11 put pm after
+        var ampm = hours >= 12 ? 'pm' : 'am';
+
+        //sets the remainder of being divided by 12 to being the new hour
+        hours = hours % 12;
+
+        //if hours equals hours display, if not put 12
+        hours = hours ? hours : 12;
+
+        return hours + ampm;
+    }
+
+
+
 });
